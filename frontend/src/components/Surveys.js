eed8f83cf6378eea822b94f2ad05970bd39682c7
manvithac <manvitha.challagundla@gmail.com>
2018-04-29 14:00:18 -0700

    Surveys Card

--- frontend/src/components/Surveys.js
+++ frontend/src/components/Surveys.js
@@ -6,7 +6,7 @@ import FloatingActionButton from 'material-ui/FloatingActionButton';
 import ContentAdd from 'material-ui/svg-icons/image/edit';
 
 const style = {
-    marginLeft: 300,
+    marginLeft: 250,
 
 };
 
@@ -17,7 +17,25 @@ class Surveys extends Component{
             surveysCreated:[
                 {
                     name : 'Customer Feedback',
-                    expiryDate:'06-06-2018'
+                    expiryDate:'06-06-2018',
+                    status:'published'
+                },
+                {
+                    name : 'Interview Feedback',
+                    expiryDate:'06-06-2018',
+                    status:'Saved'
+                }
+            ],
+            surveysToSubmit:[
+                {
+                    name : 'SJSU Library Feedback',
+                    expiryDate:'06-07-2018',
+                    status:'Saved'
+                },
+                {
+                    name : 'Event Feedback',
+                    expiryDate:'06-05-2018',
+                    status:'Submitted'
                 }
             ],
         }
@@ -36,13 +54,26 @@ class Surveys extends Component{
                                 <div className="EachSurveyBox">
                                     <div class="row">
                                         <b><div className="Questrial" style={{'font-size':'20px'}}>{card.name}</div></b>
-                                        <FloatingActionButton mini={true}
-                                                              style={style}>
-                                            <ContentAdd />
-                                        </FloatingActionButton>
+                                        {
+                                            card.status == 'Saved' ? (
+                                                <FloatingActionButton mini={true}
+                                                                      style={style}
+                                                >
+                                                    <ContentAdd />
+                                                </FloatingActionButton>
+                                            ) : (
+                                                <FloatingActionButton mini={true}
+                                                                      style={style}
+                                                                      disabled={true}
+                                                >
+                                                    <ContentAdd />
+                                                </FloatingActionButton>
+                                            )
+                                        }
                                     </div>
                                     <div class="row">
                                         <p className="Questrial" style={{'font-size':'15px'}}>Expiry: {card.expiryDate}</p>
+                                        <div className="Questrial ml-5" style={{'font-size':'15px'}}>Status: <b>{card.status}</b></div>
                                     </div>
                                 </div>
                             ))
@@ -52,6 +83,36 @@ class Surveys extends Component{
                         <h4 class="Questrial">Submitted Surveys</h4>
                         <hr style=
                                 {{'width':'50%','textAlign':'center'}}/>
+                        {
+                            this.state.surveysToSubmit.map((card, index) => (
+                                <div className="EachSurveyBox">
+                                    <div class="row">
+                                        <b><div className="Questrial" style={{'font-size':'20px'}}>{card.name}</div></b>
+                                        {
+                                            card.status == 'Saved' ? (
+                                                <FloatingActionButton mini={true}
+                                                                      style={style}
+                                                >
+                                                    <ContentAdd />
+                                                </FloatingActionButton>
+                                            ) : (
+                                                <FloatingActionButton mini={true}
+                                                                      style={style}
+                                                                      disabled={true}
+                                                >
+                                                    <ContentAdd />
+                                                </FloatingActionButton>
+                                            )
+                                        }
+
+                                    </div>
+                                    <div class="row">
+                                        <p className="Questrial" style={{'font-size':'15px'}}>Expiry: {card.expiryDate}</p>
+                                        <div className="Questrial ml-5" style={{'font-size':'15px'}}>Status: <b>{card.status}</b></div>
+                                    </div>
+                                </div>
+                            ))
+                        }
                     </div>
                 </div>
             </div>
