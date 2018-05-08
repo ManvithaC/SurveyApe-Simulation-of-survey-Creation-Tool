2200a240788d272e07774c06ae35cf00db2535a2
NIKHILA GALALA <nikhila.galala@sjsu.edu>
2018-05-08 15:39:28 -0700

    Added crossorigin

--- Backend/src/main/java/com/controller/statsController.java
+++ Backend/src/main/java/com/controller/statsController.java
@@ -3,12 +3,10 @@ package com.controller;
 import com.service.StatsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:3000", allowCredentials = "true")
 public class statsController {
     @Autowired
     private StatsService statsService;
